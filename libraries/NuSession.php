c30739a7fac456cf6b3937b096a34d627e3acf92
Noercholis <poetnoer@gmail.com>
2016-06-05 02:23:38 +0700

    use memcache

--- libraries/NuSession.php
+++ libraries/NuSession.php
@@ -2,18 +2,27 @@
 
 class NuSession{
 
+    protected $memcache;
+
+    public function __construct(){
+        $app            = getInstance();
+        $this->memcache = new Memcache;
+        $this->memcache->connect($app->config['memcache']['host'],$app->config['memcache']['port']) or die ("Could not connect");
+    }
+
     public function set($key,$value){
-        $_SESSION[$key]     = json_encode($value);
+        $this->memcache->set($key,$value);
     }
 
     public function delete($key){
-        unset($_SESSION[$key]);
+        $this->memcache->delete($key);
     }
 
     public function get($key,$default=false){
-        if(!isset($_SESSION[$key]))
+        $result = $this->memcache->get($key);
+        if(!$result)
             return $default;
-        return json_decode($_SESSION[$key]);
+        return $result;
     }
 
 }
